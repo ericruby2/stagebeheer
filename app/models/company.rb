f7601209855be59a16380e8288a72eeffba1a1bd
Eric Cornelissen <ericc@xs4all.nl>
2015-10-02 15:07:06 +0200

    na inbouwen 4 boolean velden, alleen op index view

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,5 @@
 class Company < ActiveRecord::Base
-	attr_accessible :name,:adress, :zip, :city, :abn, :ecabo, :activity, :email, :comment
+	attr_accessible :name,:adress, :zip, :city, :applicatie, :beheer, :netwerk, :ecabo, :activity, :email, :comment
 	
 	
 end
